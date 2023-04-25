12422ef086c085199a8d3628e933861fe63be874
Mohamed-Eltobgy <mohamedhassan17602@yahoo.com>
2023-04-25 03:10:03 +0200

    completed half chess

--- Ehbd.js
+++ Ehbd.js
@@ -204,53 +204,142 @@ class Chess extends engine {
         this.fcol = this.input.value.charCodeAt(1)- 97;
         this.trow = this.toInput.value.charCodeAt(0)- 49;
         this.tcol = this.toInput.value.charCodeAt(1)- 97;
-        try {
-          this.controller();
-        } catch (error) {
-          console.error(error.message);
-        }
+        this.controller();
       });
   
     document.body.appendChild(this.board);
     document.body.appendChild(this.form);
   }
-  // drawer(){
-  //     const cells = document.querySelectorAll('.cell');
-  //     for (let i = 0; i < cells.length; i++) {
-  //       const row = cells[i].dataset.row;
-  //       const col = cells[i].dataset.col;
-  //       cells[i].textContent = this.grid[row][col];
-  //     }
-  // }
-  controller() {
-    if (this.grid[this.frow][this.fcol] == ' ') {
-      alert('Invalid place');
-    }else{
-      if(this.turn == 1){ //white turn
-        if(this.grid[this.frow][this.fcol] < 1912 || this.grid[this.frow][this.fcol] > 1917 ||
-           this.trow < 0 || this.trow > 7 || this.tcol < 0 || this.tcol > 7){
-          console.log("error");
-          return;
-        }else{
-
-        }
-        this.turn = 2;
-      }else{ //black turn
-
-        this.turn = 1;
+  drawer() {
+    const cells = document.querySelectorAll('.cell');
+    for (let i = 0; i < cells.length; i++) {
+      const row = parseInt(cells[i].dataset.row);
+      const col = parseInt(cells[i].dataset.col);
+      if (this.grid[row][col] == ' ') {
+        cells[i].textContent = this.grid[row][col];
+      } else {
+        cells[i].textContent = String.fromCharCode(this.grid[row][col]);
       }
     }
-    /*else {
-      if (this.turn == 1) {
-        this.grid[this.row][this.col] = 'X';
-        this.turn = 2;
-      } else if (this.turn == 2) {
-        this.grid[this.row][this.col] = 'O';
-        this.turn = 1;
-      }
-      this.drawer();
-    }*/
   }
+  controller() {
+    let clearPath = false;
+    if (this.grid[this.frow][this.fcol] == ' ') {
+      alert('Invalid place1');
+    }else{
+      if(this.turn == 1){ //white turn
+        if(this.grid[this.trow][this.tcol] == 9813 || this.grid[this.trow][this.tcol] == 9814 ||
+          this.grid[this.trow][this.tcol] == 9815 || this.grid[this.trow][this.tcol] == 9816 ||
+          this.grid[this.trow][this.tcol] == 9816 || this.grid[this.trow][this.tcol] == 9817 || 
+          this.grid[this.trow][this.tcol] == 9812){
+           alert('Invalid move');
+           return;
+         }
+        if(this.grid[this.frow][this.fcol] < 9812 || this.grid[this.frow][this.fcol] > 9817 ||
+           this.trow < 0 || this.trow > 7 || this.tcol < 0 || this.tcol > 7){
+            alert('Invalid place2');
+            return;
+        }else{
+          var flag = 0;
+          switch(this.grid[this.frow][this.fcol]){
+              case 9817: //pawn
+                if(this.trow == this.frow-1){
+                  if(this.fcol == this.tcol){
+                    if(this.grid[this.trow][this.tcol] == ' '){
+                      flag = 1;
+                    }else{
+                      alert('Invalid place3');
+                      return;
+                    }
+                  }else if((this.fcol - this.tcol) == 1 && this.grid[this.trow][this.tcol] != ' '){
+                      flag = 1;
+                  }else if((this.tcol - this.fcol) == 1 && this.grid[this.trow][this.tcol] != ' '){
+                      flag = 1;
+                  }
+                }else if(this.trow == this.frow-2 && this.fcol == this.tcol){
+                  if(this.grid[this.trow][this.tcol] == ' ' && this.grid[this.frow-1][this.fcol] == ' '){
+                    if(this.frow == 6){
+                      flag = 1;
+                    }else{
+                      alert('Invalid place3');
+                      return;
+                    }
+                  }
+                }else{
+                  alert('invalid move');
+                }
+                if(flag == 1){
+                  this.grid[this.trow][this.tcol] = 9817;
+                  this.grid[this.frow][this.fcol] = ' ';
+                }
+                break;
+              case 9812: //King
+                if ((Math.abs(this.trow - this.frow) > 1) || (Math.abs(this.tcol - this.fcol) > 1) ||
+                    (this.trow- this.frow == 0 && this.tcol- this.fcol == 0)) {
+                  alert('Invalid move');
+                  return;
+                } else {
+                  this.grid[this.trow][this.tcol] = 9812;
+                  this.grid[this.frow][this.fcol] = ' ';
+                }
+                break;
+              case 9813: //Queen
+                if(this.trow - this.frow == 0 || this.tcol-this.fcol == 0){
+                  clearPath = this.validateMove();
+                }else if(Math.abs(this.trow - this.frow) === Math.abs(this.tcol - this.fcol)){
+                  clearPath = this.validateMoveDiagonally();
+                }
+                if (clearPath) {
+                  this.grid[this.trow][this.tcol] = 9813;
+                  this.grid[this.frow][this.fcol] = ' ';
+                } else {
+                  alert('Invalid move');
+                  return;
+                }
+              break;
+              case 9814: //ٌRooks
+                clearPath = this.validateMove();
+                  if (clearPath) {
+                    this.grid[this.trow][this.tcol] = 9814;
+                    this.grid[this.frow][this.fcol] = ' ';
+                  } else {
+                    alert('Invalid move');
+                    return;
+                  }
+              break;
+              case 9815: //bishops
+                clearPath = this.validateMoveDiagonally();
+                if (clearPath) {
+                  this.grid[this.trow][this.tcol] = 9815;
+                  this.grid[this.frow][this.fcol] = ' ';
+                } else {
+                  alert('Invalid move');
+                  return;
+                }
+              break;
+              case 9816: //Knights
+              if((Math.abs(this.tcol - this.fcol) == 1 && Math.abs(this.trow - this.frow) == 2) ||
+                  (Math.abs(this.trow - this.frow) == 1 && Math.abs(this.tcol - this.fcol) == 2) ){
+                    clearPath = true;
+              }
+                if (clearPath) {
+                  this.grid[this.trow][this.tcol] = 9816;
+                  this.grid[this.frow][this.fcol] = ' ';
+                } else {
+                  alert('Invalid move');
+                  return;
+                }
+            }
+          }
+        }
+        this.drawer();
+        //this.turn = 2;
+      }/*else{ //black turn
+
+        this.turn = 1;
+      }*/
+    }
+
   init(){
     for (let i = 0; i < this.rows; i++) {
       for (let j = 0; j < this.cols; j++) {
@@ -266,21 +355,26 @@ class Chess extends engine {
           this.grid[i][j] = 9823;
         }
         if(i == 0 ){
-          if(j == 0 || j ==7) //Rooks
+          if(j == 0 || j ==7){ //Rooks
             cell.textContent = String.fromCharCode(9820);
             this.grid[i][j] = 9820;
-          if(j == 1 || j==6)  //Knights
+          }
+          if(j == 1 || j==6){  //Knights
             cell.textContent = String.fromCharCode(9822);
             this.grid[i][j] = 9822;
-          if(j == 2 || j==5)  //Bishops
+          }
+          if(j == 2 || j==5){  //Bishops
             cell.textContent = String.fromCharCode(9821);
             this.grid[i][j] = 9821;
-          if(j == 3)  //Queen
+          }
+          if(j == 3){  //Queen
             cell.textContent = String.fromCharCode(9819);
             this.grid[i][j] = 9819;
-          if(j == 4)  //King
+          }
+          if(j == 4){  //King
             cell.textContent = String.fromCharCode(9818);
             this.grid[i][j] = 9818;
+          }
         }
         //white
         if(i == 6){
@@ -288,21 +382,26 @@ class Chess extends engine {
           this.grid[i][j] = 9817;
         }
         if(i == 7 ){
-          if(j == 0 || j ==7) //Rooks
+          if(j == 0 || j ==7){ //Rooks
             cell.textContent = String.fromCharCode(9814);
             this.grid[i][j] = 9814;
-          if(j == 1 || j==6)  //Knights
+          }
+          if(j == 1 || j==6){  //Knights
             cell.textContent = String.fromCharCode(9816);
             this.grid[i][j] = 9816;
-          if(j == 2 || j==5)  //Bishops
+          }
+          if(j == 2 || j==5){  //Bishops
             cell.textContent = String.fromCharCode(9815);
             this.grid[i][j] = 9815;
-          if(j == 3)  //Queen
+          }
+          if(j == 3){  //Queen
             cell.textContent = String.fromCharCode(9813);
             this.grid[i][j] = 9813;
-          if(j == 4)  //King
+          }
+          if(j == 4){ //King
             cell.textContent = String.fromCharCode(9812);
             this.grid[i][j] = 9812;
+          }  
         }
         if((i+j)%2 == 0){
           cell.style.backgroundColor = 'grey';
@@ -313,7 +412,79 @@ class Chess extends engine {
       }
     }
   }
+  validateMove(){
+    let clearPath = true;
+    if(this.trow - this.frow === 0) {   //move horiznotal
+      if(this.tcol > this.fcol){
+        for(let j= this.fcol+1; j<this.tcol; j++){
+          if (this.grid[this.trow][j] !== ' ') {
+            clearPath = false;
+            break;
+          }
+        }
+      }else{
+        for(let j= this.tcol+1; j<this.fcol; j++){
+          if (this.grid[this.trow][j] !== ' ') {
+            clearPath = false;
+            break;
+          }
+        }
+      }
+    }else if(this.tcol - this.fcol === 0) { // move vertical
+      let clearPath = true;
+      if(this.trow > this.frow){  //move down
+        for(let i= this.frow+1; i<this.trow; i++){
+          if (this.grid[i][this.tcol] !== ' ') {
+            clearPath = false;
+            break;
+          }
+        }
+      }else{  //move up
+        for(let i= this.trow+1; i<this.frow; i++){
+          if (this.grid[i][this.tcol] !== ' ') {
+            clearPath = false;
+            break;
+          }
+        }
+      }
+   }
+   return clearPath;
+  }
+  validateMoveDiagonally(){
+    let clearPath = true;
+      if(this.trow > this.frow && this.tcol > this.fcol){  //move down right
+        for(let i= this.frow+1, j = this.fcol+1; i<this.trow; i++, j++){
+          if (this.grid[i][j] !== ' ') {
+            clearPath = false;
+            break;
+          }
+        }
+      }else if(this.trow > this.frow && this.tcol < this.fcol){ //move down left
+        for(let i= this.frow+1, j = this.fcol+1; i<this.trow; i++, j--){
+          if (this.grid[i][j] !== ' ') {
+            clearPath = false;
+            break;
+          }
+        }
+      }else if(this.trow < this.frow && this.tcol > this.fcol){ //move up right
+        for(let i= this.frow+1, j = this.fcol+1; j<this.tcol; i--, j++){
+          if (this.grid[i][j] !== ' ') {
+            clearPath = false;
+            break;
+          }
+        }
+      }else{
+        for(let i= this.trow+1, j = this.tcol+1; j<this.tcol; i--, j--){
+          if (this.grid[i][j] !== ' ') {
+            clearPath = false;
+            break;
+          }
+        }
+      }
+      return clearPath;
+  }
 }
+
 //////////////////////////////////////////////////////////////////
 let game;
 const games = document.querySelectorAll('.game');
